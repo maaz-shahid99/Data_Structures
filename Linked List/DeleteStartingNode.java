fe56e47b31a1b53699dd036a7f1021aeb8317b62
maaz-shahid99 <goluthakle.com>
2023-07-10 13:04:59 +0530

    Code Completed

--- Linked List/DeleteStartingNode.java
+++ Linked List/DeleteStartingNode.java
@@ -2,17 +2,29 @@ import java.util.Random;
 
 public class DeleteStartingNode {
 
-    Node head;
+    Node head, tail = null;
 
     Random random = new Random();
+
+    public void deleteStartingNode() {
+        head = head.next;
+    }
 
     public void generateList(int n) {
 
         for (int i = 0; i < n; i++) {
             Node new_node = new Node();
-            new_node.data = random.nextInt(100);
-            new_node.next = head;
-            head = new_node;
+
+            if (head == null) {
+                head = new_node;
+                tail = new_node;
+                new_node.data = random.nextInt(100);
+            } else {
+                new_node.data = random.nextInt(100);
+                new_node.next = head;
+                head = new_node;
+            }
+
         }
     }
 
@@ -31,12 +43,20 @@ public class DeleteStartingNode {
                 if (trav.next != null)
                     System.out.print(trav.data + " -> ");
                 else
-                    System.out.print(trav.data);
+                    System.out.println(trav.data);
 
                 trav = trav.next;
             }
         }
+        System.out.println("Head: " + head.data+" Tail: " + tail.data );
 
+    }
+
+    public void insertTail(int data) {
+        Node new_node = new Node();
+        new_node.data = data;
+        tail.next = new_node;
+        tail = new_node;
     }
 
     public static void main(String[] args) {
@@ -45,6 +65,9 @@ public class DeleteStartingNode {
 
         list.generateList(10);
         list.printList();
-
+        // list.deleteStartingNode();
+        // list.printList();
+        list.insertTail(1001);
+        list.printList();
     }
 }
--- Linked List/Node.java
+++ Linked List/Node.java
@@ -1,4 +1,4 @@
 public class Node {
     int data;
-    Node next;
+    Node next=null;
 }
